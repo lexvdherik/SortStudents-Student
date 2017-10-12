4a69f2a86b66c0dc7e9faea33de2d51510317811
Lex van den Herik <lex-herik@hotmail.com>
2017-10-12 16:55:40 +0200

    add comments for generateGrade()

--- sort-students/src/main/java/nl/hva/dmci/ict/se/datastructures/StudentGenerator.java
+++ sort-students/src/main/java/nl/hva/dmci/ict/se/datastructures/StudentGenerator.java
@@ -14,7 +14,7 @@ import model.Student;
  */
 public class StudentGenerator {
     private int studentNumberStart= 50080001;
-    private static final int NUMBER_OF_STUDENTS = 1000;
+    private static final int NUMBER_OF_STUDENTS = 10000;
     private final Student[] studentArray = new Student[NUMBER_OF_STUDENTS];
     
     public Student[] studentGenerator(){
@@ -30,13 +30,13 @@ public class StudentGenerator {
     } 
     
     public double generateGrade(){
-        double start = 0.95;
-        double end = 10.04;
-        double random = new Random().nextDouble();
-        double result = start + (random * (end - start));
-        result = Math.round(result * 10);
-        result = result / 10; 
-        return result;
+        double start = 0.95;//minimum, 0.95 because it will be rounded;
+        double end = 10.04;//maximum 10.04 because it will be rounded;
+        double random = new Random().nextDouble();//generates a random double between 0.0 and 1.0
+        double grade = start + (random * (end - start));//calculates the random grade
+        grade = Math.round(grade * 10);//rounds up the grade*10 so there wont be to any decimals
+        grade = grade / 10;//devide bij 10 so there will be 1 decimal
+        return grade;//return the grade
     }
     
 }
